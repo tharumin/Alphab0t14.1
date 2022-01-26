253c0151c87c0a0ab985e182bda08c2548484ca9
darkdevil3610 <94959564+darkdevil3610@users.noreply.github.com>
2022-01-26 10:29:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 	'https://zeks.me': 'Alphabott',
 }
 
-global.owner = ['62887435047326','918156874290']
+global.owner = ['917025770987','917025770987']
 global.packname = setting.botname
 global.author = setting.ownername
 global.alphaVersion = [2, 2143, 3]
